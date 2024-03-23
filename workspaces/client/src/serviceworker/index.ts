a8f9d35cdd4adfd006b7b46ce47d01960388aa8b
dora <31735614+dora1998@users.noreply.github.com>
2024-03-24 01:30:40 +0900

    queue復活

--- workspaces/client/src/serviceworker/index.ts
+++ workspaces/client/src/serviceworker/index.ts
@@ -1,6 +1,13 @@
 /// <reference types="@types/serviceworker" />
 
+import PQueue from 'p-queue';
+
 import { transformJpegXLToBmp } from './transformJpegXLToBmp';
+
+// ServiceWorker が負荷で落ちないように並列リクエスト数を制限する
+const queue = new PQueue({
+  concurrency: 5,
+});
 
 self.addEventListener('install', (ev: ExtendableEvent) => {
   ev.waitUntil(self.skipWaiting());
@@ -11,6 +18,19 @@ self.addEventListener('activate', (ev: ExtendableEvent) => {
 });
 
 self.addEventListener('fetch', (ev: FetchEvent) => {
+  const url = new URL(ev.request.url);
+
+  // apiとimagesは負荷が高いのでキューに追加して処理する
+  if (url.pathname.startsWith('/images/') || url.pathname.startsWith('/api/')) {
+    console.log('queued: ', url.pathname);
+    ev.respondWith(
+      queue.add(() => onFetch(ev.request), {
+        throwOnTimeout: true,
+      }),
+    );
+    return;
+  }
+
   ev.respondWith(onFetch(ev.request));
 });
 
