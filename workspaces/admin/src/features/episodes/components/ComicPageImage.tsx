f9da4da2ae4adb3240893292fa3c033ca408e442
dora <31735614+dora1998@users.noreply.github.com>
2024-03-24 16:54:29 +0900

    webpエンコードしてた部分を修正

--- workspaces/admin/src/features/episodes/components/ComicPageImage.tsx
+++ workspaces/admin/src/features/episodes/components/ComicPageImage.tsx
@@ -15,7 +15,7 @@ export const ComicPageImage: React.FC<Props> = ({ pageImageId }) => {
     queryFn: async ({ queryKey: [, { pageImageId }] }) => {
       const image = new Image();
       image.src = getImageUrl({
-        format: 'webp',
+        format: 'avif',
         imageId: pageImageId,
       });
       await image.decode();
