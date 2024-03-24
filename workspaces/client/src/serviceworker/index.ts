92f5af0d7c8ec9cb02409004a15e67853b4966fb
dora <31735614+dora1998@users.noreply.github.com>
2024-03-24 15:04:25 +0900

    そろそろキューなしでサーバー耐えられるはず

--- workspaces/client/src/serviceworker/index.ts
+++ workspaces/client/src/serviceworker/index.ts
@@ -1,33 +1,9 @@
 /// <reference types="@types/serviceworker" />
 
-import PQueue from 'p-queue';
-
-// ServiceWorker が負荷で落ちないように並列リクエスト数を制限する
-const queue = new PQueue({
-  concurrency: 5,
-});
-
 self.addEventListener('install', (ev: ExtendableEvent) => {
   ev.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', (ev: ExtendableEvent) => {
   ev.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (ev: FetchEvent) => {
-  const url = new URL(ev.request.url);
-
-  // apiとimagesは負荷が高いのでキューに追加して処理する
-  if (url.pathname.startsWith('/images/') || url.pathname.startsWith('/api/')) {
-    console.log('queued: ', url.pathname);
-    ev.respondWith(
-      queue.add(() => fetch(ev.request), {
-        throwOnTimeout: true,
-      }),
-    );
-    return;
-  }
-
-  ev.respondWith(fetch(ev.request));
 });
